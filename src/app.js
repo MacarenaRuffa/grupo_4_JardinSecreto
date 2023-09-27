12383c50678f6bb0fedf320ad8d6d082c2cc28b8
abrilramos12 <abrilramos7@hotmail.com>
2023-09-27 20:54:27 -0300

    agregando estilos generales

--- src/app.js
+++ src/app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const path = require('path'); 
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req,res) => { res.sendFile(path.join(__dirname, '/views/index.html'))});
 
 const port=3030;
 app.listen(port, () =>{
-    console.log('Servidor iniciado en: http://localhost:${port}');
-})
+    console.log(`Servidor iniciado en: http://localhost:${port}`);
+});
