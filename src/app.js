99a83f9730c632150bffc0c7442a240f94ab7456
MacarenaRuffa <macarena.ruffa@gmail.com>
2023-10-08 22:45:45 -0300

    estetica y agrego get post en app.js

--- src/app.js
+++ src/app.js
@@ -13,6 +13,27 @@ app.get('/product-cart', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/productCart.html'));
 });
 
+app.get('/product-detail', (req, res) => { 
+    res.sendFile(path.join(__dirname, 'views/productDetail.html'));
+});
+app.post('/product-detail', (req, res) => { 
+    res.redirect('/');
+});
+
+app.get('/register', (req, res) => { 
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+app.post('/register', (req, res) => { 
+    res.redirect('/');
+});
+
+app.get('/login', (req, res) => { 
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+app.post('/login', (req, res) => { 
+    res.redirect('/');
+});
+
 const port = 3030;
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}`);
