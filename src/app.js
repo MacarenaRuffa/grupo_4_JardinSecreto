b763011be3eeab51c5c2fe8d76843fb7f82c4855
MacarenaRuffa <macarena.ruffa@gmail.com>
2023-10-25 19:19:36 -0300

    configurando app.js y ejs

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express'); 
 const path = require('path');
 
-const app = express();
-
 const mainRoutes = require('./routes/main');
 const productsRoutes = require('./routes/products');
 const usersRoutes = require('./routes/users');
 
+const app = express();
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use('/', mainRoutes);
 app.use('/products', productsRoutes);
 app.use('/users', usersRoutes);
