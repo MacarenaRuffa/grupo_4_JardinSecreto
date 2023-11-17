335d72693f6f67f959671bccc1cf1379b22c3e9d
MacarenaRuffa <macarena.ruffa@gmail.com>
2023-11-17 18:11:05 -0300

    cambio maincontroller

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,13 +5,6 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const mainController = {
     home: (req, res) => {
-        const exterior = products.filter((product) => product.category === "Exterior");
-        const interior = products.filter((product) => product.category === "Interior");
-        const macetas = products.filter((product) => product.category === "Macetas");
-        const insumos = products.filter((product) => product.category === "Insumos");
-        const herramientas = products.filter((product) => product.category === "Herramientas");
-        const semillas = products.filter((product) => product.category === "Semillas");
-        const suculentas = products.filter((product) => product.category === "Suculentas");
         const inSale = products.filter((product) => product.sale === "in-sale");
         res.render('index', { exterior, interior, macetas, insumos, herramientas, semillas, suculentas, inSale });
 
