5fb37134b522621e8c6b25a859ae82a9ef40ba32
xagustinaperalta <xagustinaperalta@gmail.com>
2023-11-17 21:22:33 -0300

    nuevas modificaciones controlador de usuario

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const productsFilePath = path.join(__dirname, '../data/user.JSON');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const userFilePath = path.join(__dirname, '../data/user.JSON');
+const user = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
 //const bcrypt = require('bcrypt');
 
 const usersController = {
@@ -19,8 +19,8 @@ const usersController = {
 			...req.body,
 			image: req.file?.filename || "default-image.png"
 		};
-		user.push(newProduct);
-		fs.writeFileSync(productsFilePath, JSON.stringify(user, null, 2));
+		user.push(newUser);
+		fs.writeFileSync(userFilePath, JSON.stringify(user, null, 2));
 		res.redirect('/');
 	}
 };
