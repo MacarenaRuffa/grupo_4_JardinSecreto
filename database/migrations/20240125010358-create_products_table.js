ec13caf7cd6fb43fdae88af0f5d1a6393524c1d5
xagustinaperalta <xagustinaperalta@gmail.com>
2024-01-25 13:39:38 -0300

    modificando async down de migraciones

--- database/migrations/20240125010358-create_products_table.js
+++ database/migrations/20240125010358-create_products_table.js
@@ -33,20 +33,10 @@ module.exports = {
         allowNull: false
     }
    
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+   
   })},
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('products');
   }
 };
