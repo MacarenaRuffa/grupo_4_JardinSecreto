05437e656a7bc3cd4b512bf66487315b7d8303c3
xagustinaperalta <xagustinaperalta@gmail.com>
2024-01-25 14:28:40 -0300

    eliminando migraciones repetidas

--- src/database/models/Role.js
+++ src/database/models/Role.js
@@ -1,35 +1,23 @@
-module.exports = (sequelize, dataTypes) => {
-    let alias = 'Roles';
-    let cols = {
-        id: {
-            type: dataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        // created_at: dataTypes.TIMESTAMP,
-        // updated_at: dataTypes.TIMESTAMP,
-        name: {
-            type: dataTypes.STRING(45),
-            allowNull: false
-        },
-        
-    };
- 
-    let config = {
-        timestamps: false,
-        createdAt: 'created_at',
-        updatedAt: 'updated_at',
-        deletedAt: false,
-        tableName: 'roles'
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
-    const Roles = sequelize.define(alias, cols, config); 
-
-    Roles.associate = function (models) {
-        Roles.belongsTo(models.User, { // models.Genre -> Genres es el valor de alias en genres.js
-            as: "user_roles",
-            foreignKey: 'roles_id'
-        })
-    }
-   
-    return Roles;
+  }
+  Role.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Role',
+  });
+  return Role;
 };
