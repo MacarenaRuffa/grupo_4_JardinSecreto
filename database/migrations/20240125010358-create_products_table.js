d7384f15744dcf00b1e20cb42c55af35e6f5b518
jorfinello <jorfinellozwicki@gmail.com>
2024-01-24 22:09:58 -0300

    Creando Migraciones

--- database/migrations/20240125010358-create_products_table.js
+++ database/migrations/20240125010358-create_products_table.js
@@ -3,29 +3,43 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('roles', {
+    await queryInterface.createTable('products', {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
+        allowNull: false
     },
     // created_at: dataTypes.TIMESTAMP,
     // updated_at: dataTypes.TIMESTAMP,
     name: {
         type: Sequelize.STRING(45),
         allowNull: false
+    },
+    description:{
+        type: Sequelize.STRING(300),
+        allowNull: false
+    },
+    price:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    image:{
+        type: Sequelize.STRING(45),
+        allowNull: true
+    },
+    sale:{
+        type: Sequelize.TINYINT(1),
+        allowNull: false
     }
-    
-    }
-
-    )
+   
     /**
      * Add altering commands here.
      *
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-  },
+  })},
 
   async down (queryInterface, Sequelize) {
     /**
