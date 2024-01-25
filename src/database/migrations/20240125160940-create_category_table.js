b525473ac7a17596a7b47ea79835354e4d26a3fd
abrilramos12 <abrilramos7@hotmail.com>
2024-01-25 14:00:10 -0300

    agregando migraciones de category

--- src/database/migrations/20240125160940-create_category_table.js
+++ src/database/migrations/20240125160940-create_category_table.js
@@ -3,32 +3,16 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('products', {
+    await queryInterface.createTable('categories', {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true,
         allowNull: false
       },
- 
+
       name: {
         type: Sequelize.STRING(45),
-        allowNull: false
-      },
-      description: {
-        type: Sequelize.STRING(300),
-        allowNull: false
-      },
-      price: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-      },
-      image: {
-        type: Sequelize.STRING(45),
-        allowNull: true
-      },
-      sale: {
-        type: Sequelize.TINYINT(1),
         allowNull: false
       },
 
@@ -41,11 +25,11 @@ module.exports = {
         type: Sequelize.DATE
       }
 
-
-    })
+    });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('products');
+    await queryInterface.dropTable('categories');
+
   }
 };
