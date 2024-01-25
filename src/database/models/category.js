8fc719090bafcea01ef781304ba18180b196e4a3
abrilramos12 <abrilramos7@hotmail.com>
2024-01-25 14:04:20 -0300

    haciendo migraciones de category

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -1,29 +1,35 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Category extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      Category.associate = function (models) {
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Category';
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true, 
+            autoIncrement: true
+        },
+        // created_at: dataTypes.TIMESTAMP,
+        // updated_at: dataTypes.TIMESTAMP,
+        name: {
+            type: dataTypes.STRING(45),
+            allowNull: false
+        },
+
+
+    };
+    let config = {
+        timestamps: false,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: false,
+        tableName: 'roles'
+    }
+    const Category = sequelize.define(alias, cols, config);
+
+    Category.associate = function (models) {
         Category.hasMany(models.Products, { // models.Genre -> Genres es el valor de alias en genres.js
             as: "category_prduct",
             foreignKey: "categories_id"
         })
     }
-    }
-  }
-  Category.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Category',
-  });
-  return Category;
+
+    return Category;
 };
--- src/database/models/roles.js
+++ src/database/models/roles.js
@@ -1,23 +1,35 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Roles extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Roles';
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // created_at: dataTypes.TIMESTAMP,
+        // updated_at: dataTypes.TIMESTAMP,
+        name: {
+            type: dataTypes.STRING(45),
+            allowNull: false
+        },
+        
+    };
+ 
+    let config = {
+        timestamps: false,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: false,
+        tableName: 'roles'
     }
-  }
-  Roles.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Roles',
-  });
-  return Roles;
+    const Roles = sequelize.define(alias, cols, config); 
+
+    Roles.associate = function (models) {
+        Roles.belongsTo(models.User, { // models.Genre -> Genres es el valor de alias en genres.js
+            as: "user_roles",
+            foreignKey: 'roles_id'
+        })
+    }
+   
+    return Roles;
 };
