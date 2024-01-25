f38f6ae2d42a5581dda1cc86dd3e20a9a1588374
xagustinaperalta <xagustinaperalta@gmail.com>
2024-01-25 15:16:31 -0300

    modificando migraciones y modelos

---
+++ src/database/migrations/20240125175514-create-user.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING(45),
+        allowNull: false
+      },
+      user_name: {
+          type: Sequelize.STRING(45),
+          allowNull: false
+      },
+      email: {
+          type: Sequelize.STRING(64),
+          allowNull: false
+      },
+      birthday: {
+          type: Sequelize.DATE,
+          allowNull: false
+      },
+      gender: {
+          type: Sequelize.STRING(45),
+          allowNull: false
+      },
+      roles_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'roles',
+          key: 'id'
+        }
+      },
+      createAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+      },
+      updateAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
--- src/database/models/Role.js
+++ src/database/models/Role.js
@@ -10,14 +10,22 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-    }
+      Roles.belongsTo(models.User, { // models.Genre -> Genres es el valor de alias en genres.js
+          as: "user_roles",
+          foreignKey: 'roles_id'
+    })
+    
+  }
   }
   Role.init({
-    name: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING(200)
+    }
   }, {
     sequelize,
     modelName: 'Role',
+    timestamps: false,
+    tableName: 'roles'
   });
   return Role;
 };
