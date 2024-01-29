5c9083620738e50518af4bcbad8b296fea441c2f
xagustinaperalta <xagustinaperalta@gmail.com>
2024-01-29 16:30:51 -0300

    completando seeders

--- src/database/seeders/20240125225036-User_products.js
+++ src/database/seeders/20240125225036-User_products.js
@@ -3,23 +3,34 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+   
+     await queryInterface.bulkInsert('Product', [{
+        name: "Alegria del hogar",
+        description: "Planta de exterior",
+        price: 600,
+        discount: 10,
+        category: "",
+        image: "PE-Alegria_del_Hogar.png",
+        sale: "in-sale",
+        isBetaMember: false
+      },
+      
+      {
+        "name": "Azalea",
+        "description": "son los arbustos de flores del género Rhododendron",
+        "price": 4500,
+        "discount": 15,
+        "category": "Exterior",
+        "image": "PE-Azalea.png",
+        "sale": "no-sale"
+      }
+      ], {});
+
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+
+     await queryInterface.bulkDelete('Product', null, {});
+     
   }
 };
--- src/database/seeders/20240125225045-Users.js
+++ src/database/seeders/20240125225045-Users.js
@@ -3,23 +3,18 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('users', [{
+      name: 'Agustina Peralta',
+      user_name: 'agperalta',
+      email: 'agustinaperalta@gmail.com',
+      password: bcript.hashSync('12345678', 10),
+      roles_id: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+     await queryInterface.bulkDelete('User', null, {});
   }
 };
