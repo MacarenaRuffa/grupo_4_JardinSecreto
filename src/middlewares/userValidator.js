2aa8cdcbd25c3f3870399eaf0564eae73941fdcd
ManuelMC98 <manuelmuract98@gmail.com>
2024-02-13 20:26:03 -0300

    login validator

--- src/middlewares/userValidator.js
+++ src/middlewares/userValidator.js
@@ -1,8 +1,15 @@
 const { body } = require('express-validator');
 
 const loginValidator = [
-    body('email').notEmpty(),
-    body('password').notEmpty()
+
+    body('email').notEmpty().withMessage('El correo electrónico no es valido'),
+
+    body('email').isEmail().withMessage('Debe ser un correo electrónico valido'),
+
+    body('password').notEmpty().withMessage('La contraseña es incorrecta'),
+
+    body('password').isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 6 caracteres'),
+
 ];
 
 module.exports = { loginValidator };
